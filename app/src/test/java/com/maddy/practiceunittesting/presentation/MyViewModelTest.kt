177c7aa0acc2fd63c969483f5c32a898e1d90307
M Isaq <maddy829019@gmail.com>
2024-02-06 02:30:51 +0530

    Testing StateFlow - asserting on the StateFlows

--- app/src/test/java/com/maddy/practiceunittesting/presentation/MyViewModelTest.kt
+++ app/src/test/java/com/maddy/practiceunittesting/presentation/MyViewModelTest.kt
@@ -14,9 +14,20 @@ class MyViewModelTest {
 
     @Test
     fun testHotFlowRepository() = runTest {
+        // Arrange
         val repository = spyk<HotFlowRepositoryImpl>()
         val viewModel = MyViewModel(repository)
 
+        // StateFlow should be considered as StateHolder
+        // While testing always try to assert on the State of the StateFlow
+        // because if you collect it as a flow and assert on that then you may miss some values.
         assertThat(viewModel.data.value).isEqualTo(0)
+
+        // Act
+        viewModel.initialize()
+
+        // Assert
+        repository.emit(1)
+        assertThat(viewModel.data.value).isEqualTo(1)
     }
 }
