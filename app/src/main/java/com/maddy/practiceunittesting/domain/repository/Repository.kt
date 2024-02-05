6579135a87d90412001fbae766c3779ab21608d1
M Isaq <maddy829019@gmail.com>
2024-02-05 16:58:09 +0530

    Injecting CoroutineDispatcher

--- app/src/main/java/com/maddy/practiceunittesting/domain/repository/Repository.kt
+++ app/src/main/java/com/maddy/practiceunittesting/domain/repository/Repository.kt
@@ -1,21 +1,23 @@
 package com.maddy.practiceunittesting.domain.repository
 
 import com.maddy.practiceunittesting.data.LocalDatabase
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class Repository(
-    private val db: LocalDatabase = LocalDatabase()
+    private val db: LocalDatabase = LocalDatabase(),
+    private val ioDispatcher: CoroutineDispatcher
 ) {
-    private val scope = CoroutineScope(Dispatchers.IO)
+    private val scope = CoroutineScope(ioDispatcher)
 
     fun initialize() {
         scope.launch { db.populate() }
     }
 
-    suspend fun fetchData(): String = withContext(Dispatchers.IO) {
+    suspend fun fetchData(): String = withContext(ioDispatcher) {
         db.read()
     }
 }
