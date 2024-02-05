aa82a339200122da412ae8bafea009918abb0f7e
M Isaq <maddy829019@gmail.com>
2024-02-05 18:46:43 +0530

    Using relaxUnitFun = true to pre-stub functions that return Unit

--- app/src/test/java/com/maddy/practiceunittesting/domain/repository/RepositoryTest.kt
+++ app/src/test/java/com/maddy/practiceunittesting/domain/repository/RepositoryTest.kt
@@ -3,8 +3,11 @@ package com.maddy.practiceunittesting.domain.repository
 import com.google.common.truth.Truth.assertThat
 import com.maddy.practiceunittesting.data.LocalDatabase
 import io.mockk.coEvery
+import io.mockk.coVerifySequence
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verifySequence
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
@@ -15,12 +18,13 @@ class RepositoryTest {
     @Test
     fun repoTest() = runTest {
         // Arrange
-        val mockDb = mockk<LocalDatabase>()
+        // val spyDb = spyk<LocalDatabase>()    // Works as a spy
+        val mockDb = mockk<LocalDatabase>(relaxUnitFun = true)
         val repository = Repository(
             db = mockDb,
             ioDispatcher = StandardTestDispatcher(testScheduler)    // always share Scheduler
         )
-        coEvery { mockDb.populate() } returns Unit
+        // coEvery { mockDb.populate() } returns Unit       // no need to stub this after setting relaxUnitFun = true
         every { mockDb.read() } returns "Hello world"
 
         // Act
@@ -30,5 +34,10 @@ class RepositoryTest {
 
         // Assert
         assertThat(data).isEqualTo("Hello world")
+
+        coVerifySequence {
+            mockDb.populate()
+            mockDb.read()
+        }
     }
 }
